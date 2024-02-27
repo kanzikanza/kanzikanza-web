2d49c38a9e49abf0646781eda54dd8ab68da650f
sangbumlikeagod <oironese@naver.com>
2024-02-27 23:18:53 +0900

    feat : 소셜로그인 (v1) 완성

--- BACKEND/rest-service/src/main/java/com/example/restservice/security/JwtIssuer.java
+++ BACKEND/rest-service/src/main/java/com/example/restservice/security/JwtIssuer.java
@@ -9,14 +9,22 @@ import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.restservice.config.kakao.KakaoApi;
+import com.example.restservice.config.kakao.KakaoApi.OAuthToken;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtIssuer {
 
     private final JwtProperties properties;
+    private final KakaoApi kakaoApi;
+    private final JwtDecoder jwtDecoder;
+
 
     public String issue(long userId, String email, List<String> roles) {
         return JWT.create()
@@ -25,5 +33,11 @@ public class JwtIssuer {
                 .withClaim("e", email)
                 .withClaim("a", roles)
                 .sign(Algorithm.HMAC256(properties.getSecretKey()));
+    }
+
+    public String kakaoIssue(OAuthToken token)
+    {
+        DecodedJWT jwt = jwtDecoder.decode(token.getId_token());
+        return jwt.getPayload();
     }
 }
