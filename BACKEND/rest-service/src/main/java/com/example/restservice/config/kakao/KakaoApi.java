0f51d6df32f3eee848cdfb7d1c9256b54bc45842
sangbumlikeagod <oironese@naver.com>
2024-03-03 21:33:09 +0900

    commit ''

--- BACKEND/rest-service/src/main/java/com/example/restservice/config/kakao/KakaoApi.java
+++ BACKEND/rest-service/src/main/java/com/example/restservice/config/kakao/KakaoApi.java
@@ -1,6 +1,5 @@
 package com.example.restservice.config.kakao;
 
-
 import org.springframework.boot.json.JsonParser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,87 +22,88 @@ import lombok.Setter;
 @Component
 public class KakaoApi {
 
-
     // public String getAccessToken(String code) {
-    //     String accessToken = "";
-    //     String refreshToken = "";
-    //     String reqUrl = "https://kauth.kakao.com/oauth/token";
+    // String accessToken = "";
+    // String refreshToken = "";
+    // String reqUrl = "https://kauth.kakao.com/oauth/token";
 
-    //     try {
-    //         RestTemplate restTemplate = new RestTemplate();
+    // try {
+    // RestTemplate restTemplate = new RestTemplate();
 
-    //         HttpHeaders headers = new HttpHeaders();
-    //         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    // HttpHeaders headers = new HttpHeaders();
+    // headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-    //         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-    //         map.add("grant_type", "authorization_code");
-    //         map.add("client_id", kakaoApiKey);
-    //         map.add("redirect_uri", kakaoRedirectUri);
-    //         map.add("code", code);
+    // MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+    // map.add("grant_type", "authorization_code");
+    // map.add("client_id", kakaoApiKey);
+    // map.add("redirect_uri", kakaoRedirectUri);
+    // map.add("code", code);
 
-    //         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+    // HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map,
+    // headers);
 
-    //         ResponseEntity<String> response = restTemplate.postForEntity(reqUrl, request, String.class);
+    // ResponseEntity<String> response = restTemplate.postForEntity(reqUrl, request,
+    // String.class);
 
-    //         log.info("[KakaoApi.getAccessToken] responseCode = {}", response.getStatusCodeValue());
+    // log.info("[KakaoApi.getAccessToken] responseCode = {}",
+    // response.getStatusCodeValue());
 
-    //         if (response.getStatusCode() == HttpStatus.OK) {
-    //             String result = response.getBody();
-    //             log.info("responseBody = {}", result);
+    // if (response.getStatusCode() == HttpStatus.OK) {
+    // String result = response.getBody();
+    // log.info("responseBody = {}", result);
 
-    //             JsonParser parser = new JsonParser();
-    //             JsonElement element = parser.parse(result);
-    //             accessToken = element.getAsJsonObject().get("access_token").getAsString();
-    //             refreshToken = element.getAsJsonObject().get("refresh_token").getAsString();
-    //         } else {
-    //             log.error("Failed to get access token. Status code: {}", response.getStatusCodeValue());
-    //         }
-    //     } catch (Exception e) {
-    //         log.error("Error while getting access token: {}", e.getMessage());
-    //         e.printStackTrace();
-    //     }
-
-    //     return accessToken;
+    // JsonParser parser = new JsonParser();
+    // JsonElement element = parser.parse(result);
+    // accessToken = element.getAsJsonObject().get("access_token").getAsString();
+    // refreshToken = element.getAsJsonObject().get("refresh_token").getAsString();
+    // } else {
+    // log.error("Failed to get access token. Status code: {}",
+    // response.getStatusCodeValue());
+    // }
+    // } catch (Exception e) {
+    // log.error("Error while getting access token: {}", e.getMessage());
+    // e.printStackTrace();
     // }
 
-    public OAuthToken getOAuthToken(String code){
+    // return accessToken;
+    // }
+
+    public OAuthToken getOAuthToken(String code) {
         String reqUrl = "https://kauth.kakao.com/oauth/token";
-    
+
         RestTemplate rt = new RestTemplate();
-    
-        //HttpHeader 오브젝트
+
+        // HttpHeader 오브젝트
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-        
-        //HttpBody 오브젝트
+
+        // HttpBody 오브젝트
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "e0fa9c3226566a2dcda49e672fe892ac");
         params.add("redirect_uri", "http://localhost:8080/auth/Oauth2/KakaoToken");
         params.add("code", code);
-    
-        //http 바디(params)와 http 헤더(headers)를 가진 엔티티
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-                new HttpEntity<>(params, headers);
-    
-        //reqUrl로 Http 요청 , POST 방식
-        ResponseEntity<String> response =
-                rt.exchange(reqUrl, HttpMethod.POST, kakaoTokenRequest, String.class);
-    
+
+        // http 바디(params)와 http 헤더(headers)를 가진 엔티티
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // reqUrl로 Http 요청 , POST 방식
+        ResponseEntity<String> response = rt.exchange(reqUrl, HttpMethod.POST, kakaoTokenRequest, String.class);
+
         String responseBody = response.getBody();
-    
+
         Gson gson = new Gson();
         OAuthToken oAuthToken = gson.fromJson(responseBody, OAuthToken.class);
-    
+
         return oAuthToken;
     }
 
-    public KakaoOpenIdToken getOpenIdToken(String code)
-    {
+    public KakaoOpenIdToken getOpenIdToken(String code) {
         Gson gson = new Gson();
         KakaoOpenIdToken kakaoOpenIdToken = gson.fromJson(code, KakaoOpenIdToken.class);
         return kakaoOpenIdToken;
     }
+
     @Getter
     @Setter
     public class OAuthToken {
@@ -113,7 +113,7 @@ public class KakaoApi {
         private int expires_in;
         private String refresh_token;
         private String scope;
-    
+
         // 생성자, 게터, 세터 등 필요한 코드 작성
     }
 
@@ -128,5 +128,5 @@ public class KakaoApi {
         private String exp;
         private String iat;
     }
-    
+
 }
