97859a08d553bc02e5181a12f41a4eb519720148
geumhyunlee <goldbutnew@gmail.com>
2024-08-27 11:43:47 +0900

    remove: 필요없는 메뉴 삭제

--- FRONTEND/src/components/Navbar/Navbar.tsx
+++ FRONTEND/src/components/Navbar/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <nav className={style.navbar}>
       <div className={style.leftMenu}>
-        <a href="/" className={style.menuLink}>홈</a>
-        <a href="/exam" className={style.menuLink}>시험 보기</a>
+        <a href="/" className={style.menuLink}>한자, 한 자</a>
+        {/* <a href="/exam" className={style.menuLink}>시험 보기</a> */}
       </div>
       <div className={style.rightMenu}>
         <a href="/login" className={style.menuLink}>로그인</a>
