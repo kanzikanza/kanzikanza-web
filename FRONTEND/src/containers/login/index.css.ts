8c4d867a6919d7d873ca2aa750aa01acd26a7db5
geumhyunlee <goldbutnew@gmail.com>
2024-08-28 18:18:40 +0900

    feat: 카카오 로그인 로직

--- FRONTEND/src/containers/login/index.css.ts
+++ FRONTEND/src/containers/login/index.css.ts
@@ -1,8 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
+	display: 'flex'
 })
 
 export const goJoin = style({
-    
+
 })
--- FRONTEND/src/containers/login/index.tsx
+++ FRONTEND/src/containers/login/index.tsx
@@ -1,14 +1,40 @@
-'use client'
+'use client';
 
+import { useEffect } from 'react';
 import { pageTitle } from "@/src/styles/common.css";
 import Image from "next/image";
 import kakaoLogin from '#/images/kakao_login_large_narrow.png';
+import Script from 'next/script';
 
 export default function Login() {
+  useEffect(() => {
+    // SDK 초기화
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init('카카오키자리');
+    }
+  }, []);
+
+  const handleKakaoLogin = () => {
+    window.Kakao.Auth.login({
+      success: function (authObj) {
+        console.log(authObj);
+      },
+      fail: function (err) {
+        console.error(err);
+      },
+    });
+  };
+
   return (
     <div>
+      <Script 
+        src="https://developers.kakao.com/sdk/js/kakao.min.js" 
+        strategy="beforeInteractive"
+      />
       <div className={pageTitle}>로그인</div>
-      <Image src={kakaoLogin} alt="카카오 로그인" />
+      <button onClick={handleKakaoLogin}>
+        <Image src={kakaoLogin} alt="카카오 로그인" />
+      </button>
     </div>
   );
 };
