9372b9fa3f4f80171a6bd878680308b7ab971f8f
geumhyunlee <goldbutnew@gmail.com>
2024-08-30 23:28:40 +0900

    feat: streak 카드 추가

--- FRONTEND/src/containers/dashboard/StreakCard.tsx
+++ FRONTEND/src/containers/dashboard/StreakCard.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import React from "react";
+import Card from "@/components/Card";
 
 import * as styles from './index.css';
 
 export default function StreakCard() {
   return (
-    <div>
-
-    </div>
+    <Card>
+      627일째 도전 중
+    </Card>
   );
 }
